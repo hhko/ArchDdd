d805760d714c449690b3142c6d4fdb3cede994c7
hhko <hyungho.ko@gmail.com>
2024-03-12 07:55:25 +0900

    로컬 빌드 스크립트 추가

---
+++ Build-Install.ps1
@@ -0,0 +1,9 @@
+# 패키지 ID                              버전           명령
+# --------------------------------------------------------------------
+# dotnet-coverage                        17.9.6        dotnet-coverage
+# dotnet-reportgenerator-globaltool      5.2.0         reportgenerator
+
+# dotnet tool install --global dotnet-coverage --version 17.10.3
+dotnet tool install --global dotnet-coverage --version 17.9.6
+
+dotnet tool list --global
