129f780771176bc2cc92fa1db0b0c7239a57b87c
hhko <hyungho.ko@gmail.com>
2024-03-13 00:37:43 +0900

    빌드 스크립트 수정

--- Build-Install.ps1
+++ Build-Install.ps1
@@ -3,7 +3,7 @@
 # dotnet-coverage                        17.9.6        dotnet-coverage
 # dotnet-reportgenerator-globaltool      5.2.0         reportgenerator
 
-# dotnet tool install --global dotnet-coverage --version 17.10.3
 dotnet tool install --global dotnet-coverage --version 17.9.6
+dotnet tool install --global dotnet-reportgenerator-globaltool --version 5.2.0
 
 dotnet tool list --global
