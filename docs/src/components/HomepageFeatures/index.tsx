f8b147b17000bb882702597b1bac7c98b5cec583
hhko <hyungho.ko@gmail.com>
2024-03-26 07:21:07 +0900

    fix: 강조 구문 수정

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -32,7 +32,7 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        코드에 감동이 있다.
+        코드에 감동이 있어야 한다.
       </>
     ),
   },
