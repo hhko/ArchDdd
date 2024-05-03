00c0deb08b3baee21cf7b52b623c02bf51c91ecd
hhko <hyungho.ko@gmail.com>
2024-05-04 08:39:25 +0900

    refactor: Controller 기본 코드 구현

--- src/ArchDdd.Adapters.Presentation/Controllers/GreetingController.cs
+++ src/ArchDdd.Adapters.Presentation/Controllers/GreetingController.cs
@@ -4,19 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ArchDdd.Adapters.Presentation.Controllers;
 
-//[ApiController]
-////[Route("api/[controller]")]
-//[ApiVersion(1.0, Deprecated = true)]
-//[ApiVersion(2.0)]
 //[Route("api/v{version:apiVersion}/[controller]")]
 public class GreetingController(ISender sender) : ApiController(sender)
-//public class GreetingController : ApiController
 {
-    //public GreetingController()
-    //{
-        
-    //}
-
     [HttpGet("{name}")]
     public IActionResult Execute(string name)
     {
--- src/ArchDdd.Adapters.Presentation/Controllers/WeatherForecastController.cs
+++ src/ArchDdd.Adapters.Presentation/Controllers/WeatherForecastController.cs
@@ -1,39 +1,38 @@
-﻿//using ArchDdd.Adapters.Presentation.Abstractions.Controllers;
-//using MediatR;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.Extensions.Logging;
+﻿using ArchDdd.Adapters.Presentation.Abstractions.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
-//namespace ArchDdd.Adapters.Presentation.Controllers;
+namespace ArchDdd.Adapters.Presentation.Controllers;
 
-////[ApiVersion("1.5", Deprecated = true)]
-////public class WeatherForecastController(ISender sender) : ApiController(sender)
-//public class WeatherForecastController : ApiController
-//{
-//    private static readonly string[] Summaries = new[]
-//    {
-//            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//        };
+//[ApiVersion("1.5", Deprecated = true)]
+public class WeatherForecastController(ISender sender) : ApiController(sender)
+{
+    private static readonly string[] Summaries = new[]
+    {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
-//    [HttpGet(Name = "GetWeatherForecast")]
-//    public IEnumerable<WeatherForecast> Get()
-//    {
-//        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-//        {
-//            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//            TemperatureC = Random.Shared.Next(-20, 55),
-//            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-//        })
-//        .ToArray();
-//    }
-//}
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
 
-//public class WeatherForecast
-//{
-//    public DateOnly Date { get; set; }
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
 
-//    public int TemperatureC { get; set; }
+    public int TemperatureC { get; set; }
 
-//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-//    public string? Summary { get; set; }
-//}
+    public string? Summary { get; set; }
+}
