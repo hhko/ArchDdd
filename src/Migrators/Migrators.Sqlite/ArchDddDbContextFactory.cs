19286f96a0f76b2a982ee1149b708420a47d00af
hhko <hyungho.ko@gmail.com>
2024-06-19 00:46:36 +0900

    refactor: IDesignTimeDbContextFactory 이해

--- src/Migrators/Migrators.Sqlite/ArchDddDbContextFactory.cs
+++ src/Migrators/Migrators.Sqlite/ArchDddDbContextFactory.cs
@@ -12,25 +12,32 @@ public sealed class ArchDddDbContextFactory : IDesignTimeDbContextFactory<ArchDd
     {
         var builder = new DbContextOptionsBuilder<ArchDddDbContext>();
 
-        // ArchDdd\src\Migrators\Migrators.Sqlite
-        //  ->
-        // ArchDdd\src\ArchDdd
-        string hostPath = Path.Combine(Directory.GetCurrentDirectory(),
-            "..", "..",
-            nameof(ArchDdd));
 
-        string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")!;
+        // dotnet ef database update -- --environment Production
+        // https://medium.com/@caio_vms_78331/applying-database-migrations-with-ef-core-in-ci-cd-pipelines-10468e11dfdd
+        // https://learn.microsoft.com/en-us/ef/core/managing-schemas/migrations/applying?WT.mc_id=DOP-MVP-4039670&tabs=dotnet-core-cli#bundles
+        // https://blog.danielpadua.dev/posts/efcore-implementing-a-multi-environment-designtimedbcontextfactory/
 
-        Console.WriteLine(hostPath);
-        Console.WriteLine(environment);
 
-        // Build config
-        IConfiguration config = new ConfigurationBuilder()
-            .SetBasePath(hostPath)
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{environment}.json", optional: true)
-            .AddEnvironmentVariables()
-            .Build();
+        //// ArchDdd\src\Migrators\Migrators.Sqlite
+        ////  ->
+        //// ArchDdd\src\ArchDdd
+        //string hostPath = Path.Combine(Directory.GetCurrentDirectory(),
+        //    "..", "..",
+        //    nameof(ArchDdd));
+
+        //string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")!;
+
+        //Console.WriteLine(hostPath);
+        //Console.WriteLine(environment);
+
+        //// Build config
+        //IConfiguration config = new ConfigurationBuilder()
+        //    .SetBasePath(hostPath)
+        //    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        //    .AddJsonFile($"appsettings.{environment}.json", optional: true)
+        //    .AddEnvironmentVariables()
+        //    .Build();
 
         // 옵션 
         // ArchDdd.db
